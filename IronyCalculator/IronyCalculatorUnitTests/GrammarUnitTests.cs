2e6b279595e758aa57f96c87d51379c95f178219
Tony Joblin <tony.joblin@silverrailtech.com>
2016-07-19 17:28:05 +1000

    Unit tests for operator precedence problem

--- IronyCalculator/IronyCalculatorUnitTests/GrammarUnitTests.cs
+++ IronyCalculator/IronyCalculatorUnitTests/GrammarUnitTests.cs
@@ -16,6 +16,44 @@ namespace IronyCalculatorUnitTests
             m_parser = CalculatorGrammar.MakeParser();
         }
 
+        private void AssertIsNumber(ParseTreeNode node, object value)
+        {
+            Assert.AreEqual("number", node.Term.Name);
+            Assert.AreEqual(value, node.Token.Value);
+        }
+
+        private void AssertIsExpression(ParseTreeNode node)
+        {
+            Assert.AreEqual("expression", node.Term.Name);
+        }
+
+        private void AssertIsNumberExpression(ParseTreeNode node, object value)
+        {
+            AssertIsExpression(node);
+            Assert.AreEqual(1, node.ChildNodes.Count);
+            AssertIsNumber(node.ChildNodes[0], value);
+        }
+
+        private void AssertIsBinaryOpExpression(ParseTreeNode node, string op)
+        {
+            AssertIsExpression(node);
+            Assert.AreEqual(3, node.ChildNodes.Count);
+            var expectedBinOp = node.ChildNodes[1];
+            AssertIsBinaryOp(expectedBinOp, op);
+        }
+
+        private void AssertIsExpressionLine(ParseTreeNode node)
+        {
+            Assert.AreEqual("expressionLine", node.Term.Name);
+        }
+
+        private void AssertIsBinaryOp(ParseTreeNode node, string op)
+        {
+            Assert.AreEqual("binaryOp", node.Term.Name);
+            Assert.AreEqual(1, node.ChildNodes.Count);
+            Assert.AreEqual(op, node.ChildNodes[0].Token.Text);
+        }
+
         [TestMethod]
         public void ParseNumber()
         {
@@ -24,16 +62,73 @@ namespace IronyCalculatorUnitTests
             var tree = m_parser.Parse(math);
 
             Assert.IsFalse(tree.HasErrors());
-            Assert.AreEqual("expressionLine", tree.Root.Term.Name);
-            var line = tree.Root;
-            Assert.AreEqual(1, line.ChildNodes.Count);
-            Assert.AreEqual("expression", line.ChildNodes[0].Term.Name);
-            var expression = line.ChildNodes[0];
-            Assert.AreEqual(1, expression.ChildNodes.Count);
-            Assert.AreEqual("number", expression.ChildNodes[0].Term.Name);
-            var number = expression.ChildNodes[0];
-            Assert.IsNotNull(number.Token);
-            Assert.AreEqual(5, number.Token.Value);
+
+            var expectedExpressionLine = tree.Root;
+            AssertIsExpressionLine(expectedExpressionLine);
+
+            var expectedNumberExpression = expectedExpressionLine.ChildNodes[0];
+
+            AssertIsNumberExpression(expectedNumberExpression, 5);
+        }
+
+        [TestMethod]
+        public void ParseAddition()
+        {
+            var math = "2 + 3";
+
+            var tree = m_parser.Parse(math);
+
+            var expressionLine = tree.Root;
+            AssertIsExpressionLine(expressionLine);
+
+            var topLevelExpression = expressionLine.ChildNodes[0];
+            AssertIsBinaryOpExpression(topLevelExpression, "+");
+
+            var expectedNumber2 = topLevelExpression.ChildNodes[0];
+            AssertIsNumberExpression(expectedNumber2, 2);
+
+            var expectedNumber3 = topLevelExpression.ChildNodes[2];
+            AssertIsNumberExpression(expectedNumber3, 3);
+        }
+
+        [TestMethod]
+        public void OperatorPrecedence_MultipleAdd_MultipleBeforeAdd()
+        {
+            // Expecting something like this:
+            //
+            //expressionLine
+            //  expression
+            //    expression
+            //      expression
+            //        2 (number)
+            //      binaryOp
+            //        * (Key symbol)
+            //      expression
+            //        3 (number)
+            //    binaryOp
+            //      + (Key symbol)
+            //    expression
+            //      4 (number)
+
+            var math = "2 * 3 + 4";
+
+            var tree = m_parser.Parse(math);
+
+            var expressionLine = tree.Root;
+            AssertIsExpressionLine(expressionLine);
+
+            var topLevelExpression = expressionLine.ChildNodes[0];
+            AssertIsBinaryOpExpression(topLevelExpression, "+");
+
+            var expectedTwoMul3Exp = topLevelExpression.ChildNodes[0];
+            AssertIsBinaryOpExpression(expectedTwoMul3Exp, "*");
+            var expectedNumber2 = expectedTwoMul3Exp.ChildNodes[0];
+            AssertIsNumberExpression(expectedNumber2, 2);
+            var expectedNumber3 = expectedTwoMul3Exp.ChildNodes[2];
+            AssertIsNumberExpression(expectedNumber3, 3);
+
+            var expectedNumber4Expr = topLevelExpression.ChildNodes[2];
+            AssertIsNumberExpression(expectedNumber4Expr, 4);
         }
     }
 }
